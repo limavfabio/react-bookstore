f0601060833ffaf80adc337194bf5891ef7ed4d8
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-09-21 15:15:41 -0300

    Refactor redux actions to use API

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,31 +1,15 @@
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 
 // Actions
+const GET_BOOKS = 'bookstore-react/books/GET_BOOKS';
 const ADD = 'bookstore-react/books/ADD';
 const REMOVE = 'bookstore-react/books/REMOVE';
 
 // Reducer
-export default function booksReducer(
-  state = [
-    {
-      title: 'First Book',
-      author: 'First Author',
-      id: uuidv4(),
-    },
-    {
-      title: 'Second Book',
-      author: 'Second Author',
-      id: uuidv4(),
-    },
-    {
-      title: 'Third Book',
-      author: 'Third Author',
-      id: uuidv4(),
-    },
-  ],
-  action = {}
-) {
+export default function booksReducer(state = [], action = {}) {
   switch (action.type) {
+    case GET_BOOKS:
+      return action.payload;
     case ADD:
       return [...state, action.book];
     case REMOVE:
@@ -36,12 +20,63 @@ export default function booksReducer(
 }
 
 // Action Creators
-export const addBook = (book) => ({
-  type: ADD,
-  book,
-});
+export const getBooks = () => async (dispatch) => {
+  const booksFetch = await fetch(
+    'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/z4gzcApO94uwV3qBSQiu/books',
+    {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    }
+  ).then((data) => data.json());
 
-export const removeBook = (book) => ({
-  type: REMOVE,
-  book,
-});
+  const books = [];
+  Object.keys(booksFetch).forEach((e) => {
+    books.push({ ...booksFetch[e][0], item_id: e });
+  });
+  dispatch({
+    type: GET_BOOKS,
+    payload: books,
+  });
+};
+
+export const addBook = (book) => async (dispatch) => {
+  await fetch(
+    'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/z4gzcApO94uwV3qBSQiu/books',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        item_id: book.item_id,
+        title: book.title,
+        author: book.author,
+        category: book.category,
+      }),
+    }
+  ).then(
+    dispatch({
+      type: ADD,
+      book,
+    })
+  );
+};
+
+export const removeBook = (book) => async (dispatch) => {
+  await fetch(
+    `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/z4gzcApO94uwV3qBSQiu/books/${book.item_id}`,
+    {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  ).then(
+    dispatch({
+      type: REMOVE,
+      book,
+    })
+  );
+};
